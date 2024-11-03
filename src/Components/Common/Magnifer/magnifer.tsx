69c0ab860c1727532f31f1914b8f6c6734c2dd3a
george-steven0 <george.steven10020@gmail.com>
2024-11-03 13:20:33 +0200

    display the magnifier-container based on the state condition/ for better performance

--- src/Components/Common/Magnifer/magnifer.tsx
+++ src/Components/Common/Magnifer/magnifer.tsx
@@ -48,39 +48,46 @@ const ImageMagnifier = ({
         setXY([x, y]);
     };
 
-    return <div className="relative inline-block w-full h-full">
-        <img
-            src={src}
-            className={className}
-            // width={width}
-            // height={height}
-            alt={alt}
-            onMouseEnter={(e) => mouseEnter(e)}
-            onMouseLeave={(e) => mouseLeave(e)}
-            onMouseMove={(e) => mouseMove(e)}
-        />
-        <div
-            style={{
-                display: showMagnifier ? '' : 'none',
-                position: 'absolute',
-                zIndex : '9999',
-                pointerEvents: 'none',
-                height: `${magnifierHeight}px`,
-                width: `${magnifierWidth}px`,
-                opacity: '1',
-                border: '1px solid lightgrey',
-                backgroundColor: 'white',
-                borderRadius: '5px',
-                backgroundImage: `url('${src}')`,
-                backgroundRepeat: 'no-repeat',
-                top: `${y - magnifierHeight / 2}px`,
-                left: `${x - magnifierWidth / 2}px`,
-                backgroundSize: `${imgWidth * zoomLevel}px ${imgHeight * zoomLevel}px`,
-                backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
-                backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
-            }}
-        />
-    </div>
+    return (
+        <div className="relative inline-block w-full h-full">
+            <img
+                src={src}
+                className={className}
+                // width={width}
+                // height={height}
+                alt={alt}
+                onMouseEnter={(e) => mouseEnter(e)}
+                onMouseLeave={(e) => mouseLeave(e)}
+                onMouseMove={(e) => mouseMove(e)}
+            />
+            {
+                showMagnifier ? 
+                    <div
+                        style={{
+                            display: showMagnifier ? '' : 'none',
+                            position: 'absolute',
+                            zIndex : '9999',
+                            pointerEvents: 'none',
+                            height: `${magnifierHeight}px`,
+                            width: `${magnifierWidth}px`,
+                            opacity: '1',
+                            border: '1px solid lightgrey',
+                            backgroundColor: 'white',
+                            borderRadius: '5px',
+                            backgroundImage: `url('${src}')`,
+                            backgroundRepeat: 'no-repeat',
+                            top: `${y - magnifierHeight / 2}px`,
+                            left: `${x - magnifierWidth / 2}px`,
+                            backgroundSize: `${imgWidth * zoomLevel}px ${imgHeight * zoomLevel}px`,
+                            backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
+                            backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+                        }}
+                    />
+
+                    : null
+            }
+        </div>
+    )
 };
 
 export default ImageMagnifier;
